c7e843094ee870714e947db3bd8d7375ac760f8b
Eric Fernberg <ericf2023@gmail.com>
2014-03-25 22:17:00 -0400

    more work on deploying app

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # Elastic Beanstalk Deploy
+
+[![GitHub version](https://badge.fury.io/gh/efernie%2Felasticbean-deploy.png)](http://badge.fury.io/gh/efernie%2Felasticbean-deploy)
+[![Dependency Status](https://gemnasium.com/efernie/elasticbean-deploy.svg)](https://gemnasium.com/efernie/elasticbean-deploy)
+
 This is very rough, I will be posting to npm soon.
+
+Right now this is specifically for node.js deployments.
+
+## Example Config File
+
+This is specifically node.js config
+
+```
+{
+  "aws": {
+    "AccessKey": "aws-access-key",
+    "SecretKey": "aws-secret",
+    "Region": "us-east-1",
+    "Bucket": "some bucket name"
+  },
+  "app": {
+    "VersionsToKeep": 25,
+    "ApplicationName": "Some App Name",
+    "Description": "Description of your app",
+    "AllEnvironments": {
+      "SolutionStackName": "64bit Amazon Linux 2014.02 running Node.js",
+      "OptionSettings": {
+        "aws:autoscaling:launchconfiguration": {
+          "Ec2KeyName": "keyname", // key name so you can ssh into your instances
+          "InstanceType": "t1.micro",
+          "IamInstanceProfile": "aws-elasticbeanstalk-ec2-role"
+        },
+        "aws:autoscaling:asg": {
+          "MinSize": 1,
+          "MaxSize": 4
+        },
+        "aws:elasticbeanstalk:application": {
+          "Application Healthcheck URL": "/"
+        },
+        "aws:elasticbeanstalk:hostmanager": {
+          "LogPublicationControl": true
+        },
+        "aws:elasticbeanstalk:container:nodejs": {
+          "NodeCommand": "npm start",
+          "ProxyServer": "none",
+          "GzipCompression": true
+        },
+        "aws:elb:policies": {
+          "Stickiness Policy": true
+        },
+        "aws:elasticbeanstalk:sns:topics": {
+          "Notification Endpoint": "name@email.com", // notifications email
+          "Notification Protocol": "email"
+        }
+      }
+    },
+    "Environments": { // this is changing
+      "Application-Prod": { // config for you different environments
+        "CnamePrefix": "application-prod",
+        "OptionSettings": {
+          "aws:elasticbeanstalk:application:environment": {
+            "MYAPP_ENV_NAME": "production",
+            "NODE_ENV": "production"
+          },
+          "aws:autoscaling:launchconfiguration": {
+            "InstanceType": "t1.micro"
+          }
+        }
+      }
+    }
+  }
+}
+
+```
--- bin/index.js
+++ bin/index.js
@@ -42,6 +42,7 @@ program
   .version('0.0.1')
   .option('-c, --config <file>', 'set config path. defaults to ' + defaultFileName)
   .option('-n, --cname <name>', 'cname to check')
+  .option('-t --templatename <name>', 'set template name')
   .option('-e, --environment <name>', 'set which environment name');
 
 program.on('--help', function () {
@@ -61,89 +62,96 @@ program.on('--help', function () {
  *     a. if it does move on to env (step 2)
  *     b. if not create the application
  *   2. Check if
+ *
+ *   last is create env with uploaded version to s3
  */
 program
   .command('init')
-  .description('Initialize ebs application')
-  .action(function ( env ) {
+  .description('Initialize ebs application, config and creates the buckets')
+  .action( function ( env ) {
     var appName = configFile.app.ApplicationName;
     if ( fileExists ) {
       application.checkApplication(elasticBeanstalk, appName)
         .then( function ( result ) {
           if ( result ) {
-            appEnvironment.checkEnv(elasticBeanstalk, appName, _.keys(configFile.app.Environments)[0])
+            // appEnvironment.checkEnv(elasticBeanstalk, appName, _.keys(configFile.app.Environments)[0])
+              appEnvironment.createConfigTemplate( elasticBeanstalk, configFile, 'Project-Ligers')
               .then( function ( result ) {
-                if ( !result ) {
-                  s3Bucket.checkBucket(s3, configFile.aws.Bucket)
-                    .then( function ( result ) {
-                      if ( !result ) {
-                        // s3Bucket.createBucket(s3, configFile.aws.Bucket, "") //configFile.aws.Region
-                        //   .then( function ( result ) {
-                        //     console.log(result);
-                        //   })
-                      } else {
-                        // appEnvironment.createEnv(elasticBeanstalk, configFile);
-
-                        // actually need
-                        s3Bucket.zipToBuffer('./')
-                          .then( function ( outputBuffer ) {
-                            var keyName = moment().unix() + 'projectliger.zip';
-                            var bucketParams = {
-                              Bucket: configFile.aws.Bucket,
-                              Key: keyName, // this is the name
-                              Body: outputBuffer,
-                              ContentType: 'application/zip',
-                              Metadata: {
-                                Name: 'projectliger.zip'
-                              }
-                            };
-                            s3.putObject(bucketParams, function ( err, data ) {
-                              if ( err ) return winston.error(err);
-                              console.log(data);
-                              var params = {
-                                ApplicationName: configFile.app.ApplicationName,
-                                VersionLabel: '0.0.1',
-                                AutoCreateApplication: true,
-                                Description: configFile.app.Description,
-                                SourceBundle: {
-                                  S3Bucket: configFile.aws.Bucket,
-                                  S3Key: keyName
-                                }
-                              };
-                              elasticBeanstalk.createApplicationVersion(params, function ( err, data ) {
-                                if ( err ) return logger.info('Error: ' + err);
-                                logger.info(data);
-                              });
-                            })
-                          })
-                      }
-                    })
-                } else {
+                if ( result === true ) {
                   return logger.info('Already Initialized');
+                } else {
+                  return logger.info('Initialized');
                 }
+
+                // if ( !result ) {
+
+                  // // should move check in to createbucket func
+                  // s3Bucket.checkBucket(s3, configFile.aws.Bucket)
+                  //   .then( function ( result ) {
+                  //     if ( !result ) {
+                  //       s3Bucket.createBucket(s3, configFile.aws.Bucket, '') // configFile.aws.Region for bucket
+                  //         .then( function ( result ) {
+                  //           console.log(result);
+                  //         })
+                  //     } else {
+                        // appEnvironment.createEnv(elasticBeanstalk, configFile)
+                        //   .then( function ( result ) { return winston.info('All Done Initializing') })
+                        //   .fail( function ( err ) { return winston.error(err) });
+
+                  //       // move this to deploy
+                  //       // s3Bucket.zipToBuffer('./')
+                  //       //   .then( function ( outputBuffer ) {
+                  //       //     var keyName = moment().unix() + 'projectliger.zip';
+                  //       //     var bucketParams = {
+                  //       //       Bucket: configFile.aws.Bucket,
+                  //       //       Key: keyName, // this is the name
+                  //       //       Body: outputBuffer,
+                  //       //       ContentType: 'application/zip',
+                  //       //       Metadata: {
+                  //       //         Name: 'projectliger.zip'
+                  //       //       }
+                  //       //     };
+                  //       //     s3.putObject(bucketParams, function ( err, data ) {
+                  //       //       if ( err ) return winston.error(err);
+                  //       //       console.log(data);
+                  //       //       var params = {
+                  //       //         ApplicationName: configFile.app.ApplicationName,
+                  //       //         VersionLabel: '0.0.1',
+                  //       //         AutoCreateApplication: true,
+                  //       //         Description: configFile.app.Description,
+                  //       //         SourceBundle: {
+                  //       //           S3Bucket: configFile.aws.Bucket,
+                  //       //           S3Key: keyName
+                  //       //         }
+                  //       //       };
+                  //       //       elasticBeanstalk.createApplicationVersion(params, function ( err, data ) {
+                  //       //         if ( err ) return logger.info('Error: ' + err);
+                  //       //         logger.info(data);
+                  //       //       });
+                  //       //     })
+                  //       //   })
+                  //     }
+                  //   })
+                // } else {
+                //   return logger.info('Initialized');
+                // }
               })
               .fail( function ( err ) {
                 return logger.error('Error: ' + err);
               })
-            // move on to next check
           } else {
-            // seperate this into a function
             var params = {
               ApplicationName: configFile.app.ApplicationName,
               Description: configFile.app.Description
             };
-            elasticBeanstalk.createApplication(params, function ( err, data ) {
-              if ( err ) {
-                return logger.error(err.message);
-              }
-              logger.info(data);
-            });
+            application.createApplication(elasticBeanstalk, params)
+              .then( function ( result ) { /* move on */ })
+              .fail( function ( err ) { return winston.error(err) });
           }
         })
         .fail( function ( err ) {
           return logger.error(err);
         });
-      // logger.info(configFile);
     } else {
       if ( program.config ) {
         logger.info('file', program.config);
@@ -162,8 +170,57 @@ program
       logger.error('Error: Need environment name flag -e or --environment to deploy unless default is set');
       return;
     } else {
+      var versionLabel = '0.0.1';
+      // envname -> _.keys(configFile.app.Environments)[0]
+
+      // check env goes avter put obj?
+      // appEnvironment.checkEnv(elasticBeanstalk, configFile.app.ApplicationName, _.keys(configFile.app.Environments)[0] )
+      //   .then( function ( result ) {
+      //     console.log(result);
+      //     var versionLabel = '0.0.1'; // have to set this from package.json file
+      //     if ( !result ) {
+      //       // create env
+      //       // carry version label from this to the createApplicationVersion
+      //       appEnvironment.createEnv(elasticBeanstalk, configFile, versionLabel, program.environment)
+      //         .then( function ( result ) { return winston.info('All Done Initializing') })
+      //         .fail( function ( err ) { return winston.error(err) });
+      //     } else {
+          var keyName = moment().unix() + '-projectliger' + '.zip';
+          s3Bucket.zipToBuffer('./', keyName)
+            .then( function ( outputBuffer ) {
+              var bucketParams = {
+                Bucket: configFile.aws.Bucket,
+                Key: keyName,
+                Body: outputBuffer,
+                ContentType: 'application/zip'
+              };
+              s3.putObject(bucketParams, function ( err, data ) {
+                if ( err ) return winston.error(err);
+                console.log(data);
+                var params = {
+                  ApplicationName: configFile.app.ApplicationName,
+                  VersionLabel: versionLabel,
+                  AutoCreateApplication: true,
+                  Description: configFile.app.Description,
+                  SourceBundle: {
+                    S3Bucket: configFile.aws.Bucket,
+                    S3Key: keyName
+                  }
+                };
+                elasticBeanstalk.createApplicationVersion(params, function ( err, data ) {
+                  if ( err ) return logger.info('Error: ' + err);
+                  logger.info(data);
+                  appEnvironment.createEnv(elasticBeanstalk, configFile, versionLabel, program.environment)
+                    .then( function ( result ) { return winston.info('All Done Initializing') })
+                    .fail( function ( err ) { return winston.error(err) });
+
+                });
+              })
+            })
+          // }
+        // })
+        // .fail( function ( err ) { return logger.error(err) });
       // upload zip and createapp version
-      logger.info('environment', program.environment);
     }
   });
 
@@ -238,80 +295,17 @@ program
           logger.info('Error: ' + err);
         });
     }
-  })
+  });
+
+program
+  .command('generateconfig')
+  .description('Generate Config File')
+  .action( function ( env ) {
+
+  });
 
 // this needs to be last
 program.parse(process.argv);
-
-// function checkEnv ( applicationName, envName ) {
-//   var deferred = Q.defer(),
-//       params = {
-//         ApplicationName: applicationName,
-//         EnvironmentNames: [
-//           envName
-//         ],
-//         IncludeDeleted: false
-//       };
-
-//   elasticBeanstalk.describeEnvironments(params, function ( err, data ) {
-//     if ( err ) return deferred.reject(err.message);
-//     if ( data ) {
-//       if ( data.Environments.length === 0 ) {
-//         deferred.resolve(false);
-//       } else {
-//         deferred.resolve(true);
-//       }
-//     }
-//   });
-
-//   return deferred.promise;
-// }
-
-// function createEnv () {
-//   var deferred = Q.defer();
-//   var params = {
-//     ApplicationName: configFile.app.ApplicationName,
-//     EnvironmentName: 'Project-Liger-Prod',
-//     VersionLabel: '0.0.1',
-//     TemplateName: 'Project-Liger'
-//   };
-
-//   elasticBeanstalk.createEnvironment(params, function ( err, data ) {
-//     if ( err ) return logger.error(err);
-//     winston.info(data);
-//     deferred.resolve(data);
-//   });
-
-//   return deferred.promise;
-// }
-
-// function mapOptions ( params ) {
-//   var deferred = Q.defer();
-
-//   _.each( configFile.app.AllEnvironments.OptionSettings, function ( settings, key ) {
-//     _.each( settings, function ( value, setKey ) {
-//       var obj = {
-//         Namespace: key
-//       };
-
-//       obj.OptionName = setKey;
-
-//       if ( typeof value === 'boolean' ) {
-//         value = '' + value;
-//       }
-
-//       if ( typeof value === 'number' ) {
-//         value += '';
-//       }
-//       obj.Value = value;
-//       params.OptionSettings.push(obj);
-//     });
-//   });
-
-//   deferred.resolve(params);
-
-//   return deferred.promise;
-// }
 
 // use validate?? maybe
 function checkFile ( fileName ) {
@@ -319,72 +313,3 @@ function checkFile ( fileName ) {
 
   return deferred.promise;
 }
-
-// function checkBucket ( bucketName ) {
-//   var deferred = Q.defer(),
-//       bucketHere = false;
-
-//   s3.listBuckets({}, function ( err, data ) {
-//     if ( err ) return winston.error(err);
-//     _.each( data.Buckets, function ( bucket ) {
-//       if ( bucket.Name === bucketName ) {
-//         bucketHere = true;
-//       }
-//     });
-
-//     deferred.resolve(bucketHere);
-//   });
-
-//   return deferred.promise;
-// }
-
-// function createBucket ( bucketName, region ) {
-//   var deferred = Q.defer();
-//   var params = {
-//     Bucket: bucketName,
-//     ACL: 'private'//,
-//     // CreateBucketConfiguration: {
-//     //   LocationConstraint: region //'us-east-1'
-//     // }
-//   };
-//   console.log(params);
-//   s3.createBucket(params, function(err, data) {
-//     if (err) return winston.error(err); deferred.reject(err);
-//     logger.info(data);
-//     deferred.resolve(true);
-//   });
-
-//   return deferred.promise;
-// }
-
-// function zipToBuffer ( folderLocation, outputFileName, zipName ) {
-//   var deferred = Q.defer(),
-//       outputPath = 'all.zip',
-//       srcDirectory = './',
-//       output = fs.createWriteStream(outputPath),
-//       zipArchive = archiver('zip'),
-//       outputBuffer;
-
-//   output.on('close', function() {
-//     logger.info('done with the zip', outputPath);
-//     outputBuffer = fs.readFileSync(outputPath);
-//     deferred.resolve(outputBuffer);
-//   });
-
-//   zipArchive.pipe(output);
-
-//   zipArchive.bulk([
-//     { src: [ '**/*' ], cwd: srcDirectory, expand: true }
-//   ]);
-
-//   zipArchive.finalize(function ( err, bytes ) {
-//     if ( err ) {
-//       winston.error(err);
-//       throw err;
-//     }
-
-//     logger.log('done:', base, bytes);
-//   });
-
-//   return deferred.promise;
-// }
