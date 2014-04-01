522a717a64fe5da83c97f9a916350a77b6376e8f
Eric Fernberg <ericf2023@gmail.com>
2014-04-01 09:25:43 -0400

    add terminate env func

--- bin/index.js
+++ bin/index.js
@@ -118,12 +118,9 @@ program
   .option('-e <name>')
   .action(function ( env ) {
     var currentTime = moment().unix(),
-        versionLabel,// = '0.0.3', // should get this from package.json or use zip file name
+        versionLabel = JSON.parse(fs.readFileSync('package.json','utf8')).version,// should get this from package.json or use zip file name
         keyName;
 
-    packageJson = JSON.parse(fs.readFileSync('package.json','utf8'));
-
-    versionLabel = packageJson.version;
     // should also check environment if in config file and on aws
     if ( !program.environment ) {
       logger.error('Error: Need environment name flag -e or --environment to deploy unless default is set');
@@ -151,7 +148,7 @@ program
                 versionLabel + 'a';
                 // need to work on this
 
-                // should re upload with a different app version just atach a letter on the end ie: 0.0.1a
+                // should re upload with a different app version just attach a letter on the end ie: 0.0.1a
                 console.log(err);
                 return logger.info('Error with Creating Application Version: ' + err);
               } else {
@@ -166,11 +163,10 @@ program
                         appEnvironment.createEnv(elasticBeanstalk, configFile, versionLabel, program.environment, program.environment)
                         .then( function ( result ) {
                           // run check for env health using program.environment
-                          appEnvironment.checkEnvStatus(elasticBeanstalk, program.environment)
+                          appEnvironment.constantHealthCheck(elasticBeanstalk, program.environment)
                             .then( function ( finResult ) {
-                              return logger.info('Finished Deploying');
+                              return logger.info('Finished Deploying New Environment');
                             }).fail( function ( err ) { return logger.error('ENV Health Status red')/* something might be wrong with your server*/ });
-                          // return logger.info('All Done Initializing');
                         })
                         .fail( function ( err ) { return logger.error('Error creating env: ' + err) });
                     } else {
@@ -178,11 +174,10 @@ program
                         // appEnvironment.updateEnvironment(elasticBeanstalk, configFile, versionLabel, program.environment, configFile.app.TemplateName)
                         appEnvironment.updateEnvironment(elasticBeanstalk, configFile, versionLabel, program.environment, program.environment)
                         .then( function ( result ) {
-                          appEnvironment.checkEnvStatus(elasticBeanstalk, program.environment)
+                          appEnvironment.constantHealthCheck(elasticBeanstalk, program.environment)
                             .then( function ( finResult ) {
-                              return logger.info('Finished Deploying');
+                              return logger.info('Finished Deploying Update');
                             }).fail( function ( err ) { return logger.error('ENV Health Status red') });
-                          // return logger.info('All Done Initializing updating')
                         })
                         .fail( function ( err ) { return logger.error(err.message) });
                     }
@@ -190,19 +185,83 @@ program
                   })
                   .fail( function ( err ) {
                     return logger.error(err);
-                  })
+                  });
               }
 
             });
-        });
+        }).fail( function ( err ) { throw new Error(err) });
     }
   });
 
 program
   .command('zdtdeploy')
-  .description('Zero downtime deploy    *** Not Implimented yet ***')
+  .description('Zero downtime deploy')
   .action( function () {
-    logger.error('Not Implimented yet');
+    var currentTime = moment().unix(),
+        oldEnvName = program.environment,
+        newEnvName = program.environment + '-0', // should run a check for dns/cname
+        versionLabel = JSON.parse(fs.readFileSync('package.json','utf8')).version,// should get this from package.json or use zip file name
+        keyName;
+
+    // should also check environment if in config file and on aws
+    if ( !program.environment ) {
+      return logger.error('Error: Need environment name flag -e or --environment to deploy unless default is set');
+    } else {
+      // should check if env is here for say staging then maybe?
+      keyName = currentTime + configFile.app.ApplicationName + '.zip'; // either in config file or package.json
+      s3Bucket.zipAndSave( s3, keyName, configFile)
+        .then( function ( saveResult ) {
+          logger.info('Saved properly');
+
+            var params = {
+              ApplicationName: configFile.app.ApplicationName,
+              VersionLabel: versionLabel,
+              AutoCreateApplication: true,
+              Description: configFile.app.Description,
+              SourceBundle: {
+                S3Bucket: configFile.aws.Bucket,
+                S3Key: keyName
+              }
+            };
+            // should check if version is already here if here append a letter to the version label
+            elasticBeanstalk.createApplicationVersion(params, function ( err, data ) {
+              if ( err ) {
+                versionLabel + 'a';
+                // need to work on this
+
+                // should re upload with a different app version just attach a letter on the end ie: 0.0.1a
+                console.log(err);
+                return logger.info('Error with Creating Application Version: ' + err);
+              } else {
+                logger.info('Created app version: On: ' + data.ApplicationVersion.DateCreated);
+                // should check env and move this into another file
+                appEnvironment.createEnv(elasticBeanstalk, configFile, versionLabel, newEnvName, program.environment, true)
+                  .then( function ( createResult ) {
+                    // console.log('create result ', createResult.newCNAME);
+                    appEnvironment.constantHealthCheck(elasticBeanstalk, newEnvName)
+                      .then( function ( finResult ) {
+                        appEnvironment.swapEnvNames( elasticBeanstalk, newEnvName, oldEnvName)
+                          .then( function ( swapResult ) {
+                            appEnvironment.constantHealthCheck(elasticBeanstalk, newEnvName)
+                              .then( function ( finSwapResult ) {
+                                return logger.info('Finished Deploying Updated Environment');
+                              }).fail( function ( err ) { throw new Error(err) });
+                          })
+                          .fail( function ( err ) { throw new Error(err) });
+                        // once new env is created switch old env to some name
+                      }).fail( function ( err ) {
+                        // this might not mean what it is for this
+                        return logger.error('ENV Health Status red');/* something might be wrong with your server*/
+                      });
+                  })
+                  .fail( function ( err ) {
+                    throw new Error(err);
+                  });
+              }
+
+            });
+        }).fail( function ( err ) { throw new Error(err) });
+    }
   });
 
 program
@@ -210,9 +269,21 @@ program
   .description('Check ENV Health')
   .action( function () {
     if ( program.environment ) {
+      // var oldEnvName = program.environment,
+      //     newEnvName = program.environment + '-1';
+      // appEnvironment.swapEnvNames( elasticBeanstalk, newEnvName, oldEnvName)
+      //   .then( function ( swapResult ) {
+      //     console.log('swap result:  ', swapResult);
+      //     return logger.info('Finished Deploying New Environment');
+      //   })
+      //   .fail( function ( err ) { throw new Error(err) });
       appEnvironment.checkEnvStatus(elasticBeanstalk, program.environment)
         .then( function ( result ) {
-          logger.info('Health Check', result);
+          if ( result ) {
+            logger.info('Health Check - Green');
+          } else {
+            logger.info('Health Check', result);
+          }
         }).fail( function ( err ) { return logger.error( err )});
     } else {
       return logger.error('Error: Need environment name flag -e or --environment to deploy unless default is set')
